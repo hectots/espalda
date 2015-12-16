7417a34c197f31d48df2e2ea0e06337a1745ca84
Hector Toledo <monkey.instinct@gmail.com>
2015-12-16 09:53:59 -0400

    Convert espalda.py into an installable script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name="Espalda",
+    version="1.0",
+    description="Scaffolding tool for Backbone.js applications",
+    author="Hector M. Toledo",
+    author_email="hector.toledo.soto@gmail.com",
+    scripts=['espalda']
+)
